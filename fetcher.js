da9d28903fc3ee11e3c06c290a4f643f81218c46
Anne-Dionido <anne.dionido@telus.com>
2022-11-24 20:07:39 -0700

    F Commit

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const fs = require('fs');
+
+const args = process.argv;
+
+let page = args[2];
+let fileName = args[3];
+
+request(page, function(error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  };
+
+  fs.writeFile(fileName, body, function(error) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    console.log(`Downloaded and saved ${body.length} bytes to ${fileName}`);
+  });
+});
